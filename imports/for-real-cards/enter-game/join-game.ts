fe080bbb2e4616c887a08fb7c083395c39c9527a
Ken Ono <ken.ono.gm@gmail.com>
2016-05-25 18:25:16 -0400

    moved from previous

---
+++ imports/for-real-cards/enter-game/join-game.ts
@@ -0,0 +1,71 @@
+import 'angular';
+import {Meteor} from 'meteor/meteor';
+import {FastCardsTopFrame} from './top-frame.ts';
+import {Component} from '../../common/ui-twbs_ng15/util';
+
+@Component(
+  {
+    module: 'fastcards',
+    selector: 'joinGame',
+    controller: JoinGame,
+    controllerAs: 'vm',
+    bindings: {
+      password: '@',
+      gameId: '@'
+    },
+    require: {
+      topFrame: '^fastCardsTopFrame'
+    },
+    template: `
+ 
+ <div class="panel panel-default">
+  <form class="form-horizontal">
+    <div class="panel-heading">
+      <h3 class="panel-title">Join Game</h3>
+    </div>
+    <div class="panel-body">
+      <div class="form-group">
+        <div class="col-xs-1"></div>
+        <label class="col-xs-4 control-label" for="game-id">Game Id:</label>
+        <div class="col-xs-7">
+          <input ng-model="vm.gameId" type="text" class="form-control" id="game-id">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-xs-1"></div>
+        <label class="col-xs-4 control-label" for="password">Password (if required):</label>
+        <div class="col-xs-7">
+          <input ng-model="vm.password" type="text" class="form-control" id="password">
+        </div>
+      </div>
+      <button type="button" ng-click="vm.joinGame()" class="btn btn-success btn-block">
+          Join Game
+      </button>
+      <button type="button" ng-click="vm.displayGame()" class="btn btn-default btn-block">
+          Display Game Table
+      </button>
+    </div>
+  </form>
+</div>
+
+    `
+})
+export class JoinGame{
+  $log:any;
+  $scope:any;
+  password:string;
+  gameId:string;
+  topFrame:FastCardsTopFrame;
+  constructor($log, $scope) {
+    this.$log = $log;
+    this.$scope = $scope;
+  }
+  joinGame() {
+    this.topFrame.constructor.navigateToHand(this.$scope, this.gameId, this.password);
+    this.topFrame.setGameDescription("New Game (id " + this.gameId + ")");
+  };
+  displayGame() {
+    this.topFrame.constructor.navigateToTable(this.$scope, this.gameId, this.password);
+    this.topFrame.setGameDescription("New Game (id " + this.gameId + ")");
+  }; 
+}
---
+++ imports/for-real-cards/enter-game/new-game.ts
@@ -0,0 +1,60 @@
+import {Deck} from '../api/models/deck.model.ts';
+import 'angular';
+import {Meteor} from 'meteor/meteor';
+import {FastCardsTopFrame} from './top-frame.ts'
+import {Component} from '../../common/ui-twbs_ng15/util';
+
+@Component(
+  {
+    module: 'fastcards',
+    selector: 'newGame',
+    controller: NewGame,
+    controllerAs: 'vm',
+    require: {
+      topFrame: '^fastCardsTopFrame'
+    },
+    template: `
+<div class="panel panel-default">
+  <form class="form-horizontal">
+    <div class="panel-heading">
+      <h3 class="panel-title">New Game</h3>
+    </div>
+    <div class="panel-body">
+      <div class="form-group">
+        <div class="col-xs-1"></div>
+        <label class="col-xs-4" for="password">Password (optional):</label>
+        <div class="col-xs-7">
+          <input class="form-control" ng-model="vm.password" type="text" id="password">
+        </div>
+      </div>
+      <button type="button" class="btn btn-success btn-block" ng-click="vm.newGame()">
+        Start Game
+      </button>
+      </div>
+  </form>
+</div>
+`
+  }
+)
+export class NewGame{
+  $log:any;
+  $scope:any;
+  topFrame:FastCardsTopFrame;
+  selectedDeck = 0;
+  constructor($log, $scope) {
+    this.$log = $log;
+    this.$scope = $scope;
+  }
+  password: string;
+  newGame() {
+    Meteor.call('FastCardsNewGame', this.password, (error, result)=>{
+      if (error) {
+        this.$log.error(error);
+      } else {
+        this.topFrame.constructor.navigateToHand(this.$scope, result, this.password);
+        this.topFrame.setGameDescription("New Game (id " + result + ")");
+      }
+    });
+  }
+}
+
