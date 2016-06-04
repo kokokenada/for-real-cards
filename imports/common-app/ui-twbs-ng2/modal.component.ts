692918352dcb9d66b6908b989d101dc9778a2b96
Ken Ono <ken.ono.gm@gmail.com>
2016-06-04 19:40:03 -0400

    remove unneed includes

--- imports/common-app/ui-twbs-ng2/modal.component.ts
+++ imports/common-app/ui-twbs-ng2/modal.component.ts
@@ -1,7 +1,5 @@
 import { Component, ViewContainerRef } from "@angular/core"
 
-import { MODAL_DIRECTVES, BS_VIEW_PROVIDERS } from 'ng2-bootstrap/ng2-bootstrap';
-
 import { ModalOutlet} from "../ui-ng2/modal/modal-outlet.component";
 import { ModalEvent, ModalEventType} from "../ui-ng2/modal/modal-event.class";
 import { ModalService } from "../ui-ng2/modal/modal.service"
@@ -19,8 +17,7 @@ import { ModalService } from "../ui-ng2/modal/modal.service"
   </div>
 </div>
 `,
-  directives: [ModalOutlet, MODAL_DIRECTVES],
-  viewProviders:[BS_VIEW_PROVIDERS]
+  directives: [ModalOutlet],
 })
 export class ModalDialog {
   creationModalEvent:ModalEvent;
