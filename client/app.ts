223380b13fd48e045e979e371cbe1485dc0ae63d
Ken Ono <ken.ono.gm@gmail.com>
2016-06-06 00:02:53 -0400

    work around max stack size problem (angular2-meteor/issues/273)

--- client/app.ts
+++ client/app.ts
@@ -2,8 +2,8 @@
  * Created by kenono on 2016-05-24.
  */
 import 'reflect-metadata';
-import 'zone.js/dist/zone';
-import { Component, provide } from '@angular/core';
+//import 'zone.js/dist/zone';
+import { provide } from '@angular/core';
 import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES } from '@angular/router';
 import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
 import { bootstrap } from '@angular/platform-browser-dynamic';
