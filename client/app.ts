f1fe10b24657fcc66c3ec612945a952c558f65da
Ken Ono <ken.ono.gm@gmail.com>
2016-06-07 10:27:01 -0400

    updated packages and imports

--- client/app.ts
+++ client/app.ts
@@ -1,18 +1,21 @@
 import 'reflect-metadata';
-import { Meteor } from 'meteor/meteor';
-declare var cordova:any;
-declare var require:any;
-if (Meteor.isCordova && cordova.platformId==="android") {
+import 'zone.js/dist/zone';
+/*
+ import { Meteor } from 'meteor/meteor';
+ declare var cordova:any;
+ declare var require:any;
+if (!(Meteor.isCordova && cordova.platformId==="android")) {
   // https://github.com/Urigo/angular2-meteor/issues/273
   console.log("require 'zone.js/dist/zone'");
   require('zone.js/dist/zone');
 }
+*/
 import { provide } from '@angular/core';
 import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES } from '@angular/router';
 import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
 import { bootstrap } from '@angular/platform-browser-dynamic';
 
-import {ForRealCardsTopFrame} from "../imports/for-real-cards";
+import {ForRealCardsTopFrame} from "../imports/for-real-cards/index";
 
 bootstrap(ForRealCardsTopFrame,
     [
