cd9883249ce2871f3c2406b589be3f7250d6bc64
Ken Ono <ken.ono.gm@gmail.com>
2016-06-07 07:44:26 -0400

    work around bug #273

--- client/app.ts
+++ client/app.ts
@@ -1,8 +1,12 @@
-/**
- * Created by kenono on 2016-05-24.
- */
 import 'reflect-metadata';
-//import 'zone.js/dist/zone';
+import { Meteor } from 'meteor/meteor';
+declare var cordova:any;
+declare var require:any;
+if (Meteor.isCordova && cordova.platformId==="android") {
+  // https://github.com/Urigo/angular2-meteor/issues/273
+  console.log("require 'zone.js/dist/zone'");
+  require('zone.js/dist/zone');
+}
 import { provide } from '@angular/core';
 import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES } from '@angular/router';
 import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
