17bb9924a5596d8130371e61cd2d892594a36eea
Ken Ono <ken.ono.gm@gmail.com>
2016-06-09 18:58:06 -0400

    fix app ID, experiments with access

--- mobile-config.js
+++ mobile-config.js
@@ -1,5 +1,5 @@
 App.info({
-  id: 'com.fabrica.forrealcards',
+  id: 'com.id1c9m5hkmxjq4b11holit.',
   name: 'For Real Cards',
   version: "0.0.2",
   author: "Ken Ono, Fabrica Technology",
@@ -8,3 +8,6 @@ App.info({
 });
 App.setPreference('android-versionCode', '2');
 App.accessRule("*");
+App.accessRule("https://for-real-cards.scalingo.io");
+App.accessRule("https://for-real-cards.fabrica.tech");
+App.accessRule('data:*', { type: 'navigation' });
