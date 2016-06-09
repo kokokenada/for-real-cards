18980112f539a77ca88233bd7d305599fefca04c
Ken Ono <ken.ono.gm@gmail.com>
2016-06-09 09:36:27 -0400

    mobile build clean up

---
+++ mobile-config.js
@@ -0,0 +1,10 @@
+App.info({
+  id: 'com.fabrica.forrealcards',
+  name: 'For Real Cards',
+  version: "0.0.2",
+  author: "Ken Ono, Fabrica Technology",
+  email: "ken@fabrica.tech",
+  website:"http://fabrica.tech"
+});
+App.setPreference('android-versionCode', '2');
+App.accessRule("*");
