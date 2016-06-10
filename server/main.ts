7e1b16362c08280951a145df5a78b00d5cbcd550
Ken Ono <ken.ono.gm@gmail.com>
2016-06-10 18:38:40 -0400

    attempt to fix graphics in production with browser-policy

--- server/main.ts
+++ server/main.ts
@@ -3,7 +3,18 @@ import * as log from 'loglevel';
 import "/imports/common-app/api/index.ts"
 import "/imports/for-real-cards/api/index.ts"
 
+declare var BrowserPolicy;
 
 log.setLevel(0, true)
-log.debug('log leve set to 0')
-
+// server/BroswerPolicyAndCORS.js
+Meteor.startup(function() {
+  console.log('Configuring content-security-policy');
+  BrowserPolicy.content.allowSameOriginForAll();
+  BrowserPolicy.content.allowOriginForAll('http://192.168.0.92');
+  BrowserPolicy.content.allowOriginForAll('https://for-real-cards.scalingo.io');
+  BrowserPolicy.content.allowOriginForAll('https://fabrica.scalingo.io');
+  BrowserPolicy.content.allowOriginForAll('http://for-real-cards.scalingo.io');
+  BrowserPolicy.content.allowOriginForAll('http://fabrica.scalingo.io');
+  BrowserPolicy.content.allowEval();
+  BrowserPolicy.framing.disallow();
+});
