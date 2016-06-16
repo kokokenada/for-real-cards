8ec49630ffd9f8a092688cd88cdc71dd28be1a93
Ken Ono <ken.ono.gm@gmail.com>
2016-06-16 19:30:46 -0400

    see if this fixes production mobile

--- server/main.ts
+++ server/main.ts
@@ -6,16 +6,14 @@ import "/imports/for-real-cards/api/index.ts"
 declare var BrowserPolicy;
 
 log.setLevel(0, true)
-/* / server/BroswerPolicyAndCORS.js
 Meteor.startup(function() {
   console.log('Configuring content-security-policy');
   BrowserPolicy.content.allowSameOriginForAll();
   BrowserPolicy.content.allowOriginForAll('http://192.168.0.92');
   BrowserPolicy.content.allowOriginForAll('https://for-real-cards.scalingo.io');
-  BrowserPolicy.content.allowOriginForAll('https://fabrica.scalingo.io');
+  BrowserPolicy.content.allowOriginForAll('https://for-real-cards.fabrica.tech');
   BrowserPolicy.content.allowOriginForAll('http://for-real-cards.scalingo.io');
-  BrowserPolicy.content.allowOriginForAll('http://fabrica.scalingo.io');
+  BrowserPolicy.content.allowOriginForAll('http://for-real-cards.fabrica.tech');
   BrowserPolicy.content.allowEval();
   BrowserPolicy.framing.disallow();
 });
-*/
