eba90330913150ed6b4df522098b0dcf232d1394
Ken Ono <ken.ono.gm@gmail.com>
2016-06-16 19:13:05 -0400

    bumped version

--- mobile-config.js
+++ mobile-config.js
@@ -1,12 +1,12 @@
 App.info({
   id: 'com.fabrica.forrealcards1',
   name: 'For Real Cards',
-  version: "0.0.2",
+  version: "0.0.3",
   author: "Ken Ono, Fabrica Technology",
   email: "ken@fabrica.tech",
   website:"http://fabrica.tech"
 });
-App.setPreference('android-versionCode', '2');
+App.setPreference('android:versionCode', 3);
 App.accessRule("*");
 App.accessRule("https://for-real-cards.scalingo.io");
 App.accessRule("https://for-real-cards.fabrica.tech");
