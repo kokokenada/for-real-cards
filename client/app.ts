55ed24b5b8ee6e54af2de7bcccf333d3ca5de81c
Ken Ono <ken.ono.gm@gmail.com>
2016-06-16 19:28:56 -0400

    set log level and productio mode

--- client/app.ts
+++ client/app.ts
@@ -1,16 +1,9 @@
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import { Meteor } from 'meteor/meteor';
-/*
- declare var cordova:any;
- declare var require:any;
-if (!(Meteor.isCordova && cordova.platformId==="android")) {
-  // https://github.com/Urigo/angular2-meteor/issues/273
-  console.log("require 'zone.js/dist/zone'");
-  require('zone.js/dist/zone');
-}
-*/
-import { provide } from '@angular/core';
+import * as log from 'loglevel';
+
+import { provide, enableProdMode } from '@angular/core';
 import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES } from '@angular/router';
 import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
 import {ForRealCardsTopFrame} from "../imports/for-real-cards/index";
@@ -19,6 +12,15 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 
 console.log("In apps.ts @" + new Date());
 console.log(Meteor.absoluteUrl());
+
+if (Meteor.isProduction) {
+  console.log("Production environment detected.  Log level set to error");
+  log.setLevel(LogLevel.ERROR);
+  enableProdMode();
+} else {
+  console.log("Development environment detected.  Log level set to info");
+  log.setLevel(LogLevel.INFO);
+}
 
 Meteor.setTimeout(()=>{
   bootstrap(ForRealCardsTopFrame,
