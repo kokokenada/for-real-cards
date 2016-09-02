02200595b48a3c335602086825ced1262a562f7b
Ken Ono <ken.ono.gm@gmail.com>
2016-09-02 06:44:23 -0400

    added Roles typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/alanning:roles/index.d.ts" />
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/loglevel/index.d.ts" />
 /// <reference path="globals/meteor-roles/index.d.ts" />
@@ -5,8 +6,3 @@
 /// <reference path="globals/simple-schema/simple-schema.d.ts" />
 /// <reference path="globals/underscore/index.d.ts" />
 /// <reference path="modules/es6-promise/index.d.ts" />
-
-declare module "*.html" {
-  const template: string;
-  export default template;
-}
