60fc0540adb808425ef543a641febb86fa04aef8
Ken Ono <ken.ono.gm@gmail.com>
2016-09-24 15:49:43 -0400

    formatting

--- imports/common-app/src/ui/redux/login/login.module.ts
+++ imports/common-app/src/ui/redux/login/login.module.ts
@@ -1,10 +1,10 @@
 import {Injectable} from "@angular/core";
 
-import { ReduxModule} from '../redux-module.class';
-import {loginReducer} from "./login-reducer";
-import {LoginAsync} from "./login-async.class";
-import {IAppState} from "../state.interface";
-import {LoginActions} from "./login-actions.class";
+import { ReduxModule } from '../redux-module.class';
+import { loginReducer } from "./login-reducer";
+import { LoginAsync } from "./login-async.class";
+import { IAppState } from "../state.interface";
+import { LoginActions } from "./login-actions.class";
 
 @Injectable()
 export class LoginModule extends ReduxModule<IAppState>  {
