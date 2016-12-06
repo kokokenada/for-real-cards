60de9a5cf40ed81a790309ff616ea61dcaf357bc
Ken Ono <ken.ono.gm@gmail.com>
2016-12-05 20:49:28 -0500

    remove unnecessary reference

--- imports/common-app/src/ui/redux/redux-module-util.ts
+++ imports/common-app/src/ui/redux/redux-module-util.ts
@@ -1,16 +1,16 @@
 import {IActionError, IPayloadAction} from "./action.interface";
-
+import {fromJS, Map} from 'immutable'
 export class ReduxModuleUtil {
 
   static errorFactory(actionType:string, error:IActionError):IPayloadAction {
     return {type: actionType, error: error };
   }
 
-  static arrayToMap<T>(arr:any[], key:string="_id"):Immutable.Map<string, T> {
+  static arrayToMap<T>(arr:any[], key:string="_id"):Map<string, T> {
     let obj:Object = {};
     arr.forEach( (item:any)=>{
       obj[key] = item;
     });
-    return Immutable.fromJS(obj);
+    return fromJS(obj);
   }
 }
