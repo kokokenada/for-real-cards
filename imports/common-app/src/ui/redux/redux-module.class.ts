c8e694efca00b06c7af544eae93cf610a4760503
Ken Ono <ken.ono.gm@gmail.com>
2017-03-24 20:56:32 -0400

    fix warning

--- imports/common-app/src/ui/redux/redux-module.class.ts
+++ imports/common-app/src/ui/redux/redux-module.class.ts
@@ -4,7 +4,7 @@ import { Epic } from 'redux-observable';
 
 export abstract class ReduxModule<STATE, ACTION> {
   abstract reducers:{name: string, reducer:Reducer<STATE>}[];
-  epics:Epic<ACTION>[]=[];        // Stream based middleware
+  epics:Epic<ACTION, STATE>[]=[];        // Stream based middleware
   middlewares:any[]=[];   // Normal redux middleware
   enhancers:any[]=[];
   actions:Object;
