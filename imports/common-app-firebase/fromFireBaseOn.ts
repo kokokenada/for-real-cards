7ae00d851f3603e034f92155aec5d3874f8b7fa1
Ken Ono <ken.ono.gm@gmail.com>
2017-05-23 13:06:04 -0400

    remove logging

--- imports/common-app-firebase/fromFireBaseOn.ts
+++ imports/common-app-firebase/fromFireBaseOn.ts
@@ -5,36 +5,23 @@ import {Subscriber} from 'rxjs/Subscriber';
 
 export function fromFireBaseOn<T>(ref: firebase.database.Reference): Observable<IDocumentChange<T>> {
   return Observable.create((observer: Subscriber<IDocumentChange<T>>) => {
-/*    ref.orderByKey().on('value', (snapshot) => {
+
+    ref.orderByKey().on('child_added', (snapshot: any) => {
       let doc = snapshot.val();
-      console.log('value')
-      console.log(doc)
-    });
-    */
-    ref.orderByKey().on('child_added', (snapshot: any, previous) => {
-      let doc = snapshot.val();
-      /*
-      console.log('child_added')
-      console.log(doc)
-      console.log(previous)
-      */
       observer.next({
         changeType: EDocumentChangeType.NEW,
         newDocument: doc
       });
-
     });
+
     ref.orderByKey().on('child_removed', (snapshot: any) => {
       let doc = snapshot.val();
-      console.log('child_removed')
-      console.log(doc)
       observer.next({
         changeType: EDocumentChangeType.REMOVED,
         oldDocument: doc
       });
-
-
     });
+
     ref.orderByKey().on('child_changed', (snapshot: any) => {
       let doc = snapshot.val();
       console.log('child_changed')
